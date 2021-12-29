ac8ec0d5bba64028c4a6d0d077a6eb9816b0df5d
1313ou <1313ou@gmail.com>
2021-12-29 11:44:27 +0100

    simplification

--- src/main/java/org/oewntk/wndb/in/Parser.java
+++ src/main/java/org/oewntk/wndb/in/Parser.java
@@ -5,7 +5,6 @@
 package org.oewntk.wndb.in;
 
 import org.oewntk.model.CoreModel;
-import org.oewntk.model.Key;
 import org.oewntk.model.Lex;
 import org.oewntk.parse.DataParser;
 import org.oewntk.parse.IndexParser;
@@ -19,7 +18,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.*;
-import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -27,8 +25,6 @@ import static java.util.stream.Collectors.*;
 
 public class Parser
 {
-	static final boolean FAIL_DUPLICATES = false;
-
 	// PRINT STREAMS
 
 	/**
@@ -44,13 +40,13 @@ public class Parser
 	/**
 	 * Key which is to represent sense
 	 */
-	static class Key3
+	static class Key
 	{
 		public final String lemma;
 		public final char pos;
 		public final long offset;
 
-		public Key3(final String lemma, final char pos, final long offset)
+		public Key(final String lemma, final char pos, final long offset)
 		{
 			this.lemma = lemma;
 			this.pos = pos;
@@ -60,7 +56,7 @@ public class Parser
 		@Override
 		public String toString()
 		{
-			return "Key{" + "lemma='" + lemma + '\'' + ", pos=" + pos + ", offset=" + offset + '}';
+			return "K{" + "lemma='" + lemma + '\'' + ", pos=" + pos + ", offset=" + offset + '}';
 		}
 
 		@Override
@@ -74,7 +70,7 @@ public class Parser
 			{
 				return false;
 			}
-			Key3 key = (Key3) o;
+			Key key = (Key) o;
 			return pos == key.pos && offset == key.offset && lemma.equals(key.lemma);
 		}
 
@@ -85,103 +81,6 @@ public class Parser
 		}
 	}
 
-	/**
-	 * Key which is to represent sense
-	 */
-	static class Key4
-	{
-		public final String lemma;
-		public final char pos;
-		public final long offset;
-		public final int index;
-
-		public Key4(final String lemma, final char pos, final long offset, final int index)
-		{
-			this.lemma = lemma;
-			this.pos = pos;
-			this.offset = offset;
-			this.index = index;
-		}
-
-		@Override
-		public String toString()
-		{
-			return "K{" + "lemma='" + lemma + '\'' + ", pos=" + pos + ", index=" + index + ", offset=" + offset + '}';
-		}
-
-		@Override
-		public boolean equals(final Object o)
-		{
-			if (this == o)
-			{
-				return true;
-			}
-			if (o == null || getClass() != o.getClass())
-			{
-				return false;
-			}
-			Key4 key = (Key4) o;
-			return pos == key.pos && offset == key.offset && index == key.index && lemma.equals(key.lemma);
-		}
-
-		@Override
-		public int hashCode()
-		{
-			return Objects.hash(lemma, pos, index, offset);
-		}
-	}
-
-	/**
-	 * Collected data
-	 */
-	static class Triplet<T, U, V>
-	{
-		public final T first;
-		public final U second;
-		public final V third;
-
-		public Triplet(final T first, final U second, final V third)
-		{
-			this.first = first;
-			this.second = second;
-			this.third = third;
-		}
-
-		public static <T, U, V> Triplet<T, U, V> merge3(Triplet<T, U, V> t1, Triplet<T, U, V> t2, BiFunction<V, V, V> merge)
-		{
-			assert Objects.equals(t1.first, t2.first);
-			assert Objects.equals(t1.second, t2.second);
-			return new Triplet<>(t1.first, t1.second, merge.apply(t1.third, t2.third));
-		}
-
-		@Override
-		public boolean equals(final Object o)
-		{
-			if (this == o)
-			{
-				return true;
-			}
-			if (o == null || getClass() != o.getClass())
-			{
-				return false;
-			}
-			Triplet<?, ?, ?> triplet = (Triplet<?, ?, ?>) o;
-			return first.equals(triplet.first) && second.equals(triplet.second) && third.equals(triplet.third);
-		}
-
-		@Override
-		public int hashCode()
-		{
-			return Objects.hash(first, second, third);
-		}
-
-		@Override
-		public String toString()
-		{
-			return "(" + first + "," + second + "," + third + ')';
-		}
-	}
-
 	// MAPS
 
 	// final results
@@ -189,7 +88,7 @@ public class Parser
 	/**
 	 * Lexical units
 	 */
-	private final Map<Key.W_P, Lex> lexesByKey = new TreeMap<>();
+	private final Map<org.oewntk.model.Key.W_P, Lex> lexesByKey = new TreeMap<>();
 
 	/**
 	 * Senses
@@ -213,21 +112,29 @@ public class Parser
 	/**
 	 * Sensekey by key
 	 */
-	private final Map<Key3, String> sensekeyByKey = new HashMap<>();
+	private final Map<Key, String> sensekeyByKey = new HashMap<>();
 
 	/**
 	 * Sense relations by key
 	 */
-	private final Map<Key4, Relation[]> senseRelationsByKey = new HashMap<>();
+	private final Map<Key, Relation[]> relationsByKey = new HashMap<>();
 
 	/**
-	 * Triplet (synsetId, senseIndex, tagCnt) by key
+	 * TagCnt by key representing sense
 	 */
-	private final Map<Key4, Triplet<Long, Integer, Integer>> dataByKey = new HashMap<>();
+	private final Map<Key, TagCnt> tagCntByKey = new HashMap<>();
+
+	/**
+	 * Map frame id (via, ...) to numeric id
+	 */
+	private static final Map<Integer, String> VERB_FRAME_NID_TO_IDS = Stream.of(org.oewntk.model.VerbFrame.VALUES).collect(toMap(data -> (Integer) data[2], data -> (String) data[0]));
 
 	// 1 - C O N S U M E   S Y N S E T   P O J O S
 	// from data.(noun|verb|adj|adv)
 
+	/**
+	 * Synset consumer
+	 */
 	private final Consumer<Synset> synsetConsumer = synset -> {
 
 		String synsetId = synset.synsetId.toString();
@@ -245,6 +152,12 @@ public class Parser
 		pojoSynsetsById.put(synset.synsetId, synset);
 	};
 
+	/**
+	 * Build synset relations
+	 *
+	 * @param relations relations
+	 * @return map type to set of target synset ids
+	 */
 	private static Map<String, Set<String>> buildSynsetRelations(final Relation[] relations)
 	{
 		if (relations == null || relations.length == 0)
@@ -260,6 +173,9 @@ public class Parser
 	// 2 - C O N S U M E   S E N S E   P O J O S
 	// from index.sense
 
+	/**
+	 * Sense consumer
+	 */
 	private final Consumer<Sense> senseConsumer = sense -> {
 
 		// sensekey
@@ -268,64 +184,42 @@ public class Parser
 		// key
 		String lemma = sense.lemma.toString();
 		char pos = sense.synsetId.getPos().toChar();
-		int posIndex = sense.sensePosIndex;
-		Key3 k3 = new Key3(lemma, pos, sense.synsetId.getOffset());
-		Key4 k4 = new Key4(lemma, pos, sense.synsetId.getOffset(), posIndex);
+		Key key = new Key(lemma, pos, sense.synsetId.getOffset());
 
 		// store sensekey by key
-		sensekeyByKey.put(k3, sensekey);
+		sensekeyByKey.put(key, sensekey);
 
-		// store synsetid, index, tagcnt by key
-		long synsetId = sense.synsetId.getOffset();
-		int senseIndex = sense.sensePosIndex;
-		int tagCnt = sense.tagCnt.getTagCount();
-		var triplet = new Triplet<>(synsetId, senseIndex, tagCnt);
-		if (FAIL_DUPLICATES)
+		// store tagcnt by key
+		TagCnt tagCnt = sense.tagCnt;
+		var existingTagCnt = tagCntByKey.put(key, tagCnt);
+		if (existingTagCnt != null && !existingTagCnt.equals(tagCnt))
 		{
-			assert !dataByKey.containsKey(k4) : k4 + " contains " + dataByKey.get(k4) + ", new " + triplet;
-			dataByKey.put(k4, triplet);
-		}
-		else
-		{
-			if (dataByKey.containsKey(k4))
-			{
-				var t1 = dataByKey.get(k4);
-				if (t1.equals(triplet))
-				{
-					psi.printf("data[%s] contained identical data %s, skipped%n", k4, t1);
-				}
-				else
-				{
-					var t2 = Triplet.merge3(t1, triplet, Math::max);
-					pse.printf("data[%s] contained data %s, merged with %s to %s%n", k4, t1, triplet, t2);
-					dataByKey.put(k4, t2);
-				}
-			}
-			else
-			{
-				dataByKey.put(k4, triplet);
-			}
+			// merge
+			var tagCnt2 = new TagCnt(Math.min(tagCnt.senseNum, existingTagCnt.senseNum), Math.max(tagCnt.tagCount, existingTagCnt.tagCount));
+			tagCntByKey.put(key, tagCnt2);
+			psi.printf("Tag count for %s contained %s, merged to %s%n", key, existingTagCnt, tagCnt2);
 		}
 
 		// store relations by key
 		Synset synset = pojoSynsetsById.get(sense.synsetId);
-		senseRelationsByKey.put(k4, synset.relations);
+		/* var r =*/
+		relationsByKey.put(key, synset.relations);
+		//assert r2 != null : r2;
 	};
 
 	// 3 - C O N S U M E   I N D E X   P O J O S
 	// from index.(noun|verb|adj|adv)
 
-	public static final Map<Integer, String> VERB_FRAME_NID_TO_IDS = Stream.of(org.oewntk.model.VerbFrame.VALUES).collect(toMap(data -> (Integer) data[2], data -> (String) data[0]));
-
-	//private static final Consumer<Index> indexConsumer = Tracing.psInfo::println;
-
+	/**
+	 * Index consumer
+	 */
 	private final Consumer<Index> indexConsumer = idx -> {
 
 		String lemma = idx.lemma.toString();
 		char pos = idx.pos.toChar();
 
 		// senses
-		final int[] i = {0};
+		final int[] i = {1};
 		Arrays.stream(idx.senses) //
 				.forEach(sense -> {
 
@@ -346,37 +240,36 @@ public class Parser
 								String memberLemma = member.toString();
 
 								// key
-								Key3 k3 = new Key3(lemma, pos, sense.synsetId.getOffset());
-								Key4 k4 = new Key4(lemma, pos, sense.synsetId.getOffset(), i[0]);
+								Key key = new Key(lemma, pos, sense.synsetId.getOffset());
 
-								// retrieve data
-								var data = dataByKey.get(k4);
-								if (FAIL_DUPLICATES)
-								{
-									assert i[0] == data.second : k4 + " " + data.second;
-									assert synsetId.getOffset() == data.first : k4 + " " + data.first;
-								}
+								// retrieve tag count
+								var tagCnt = tagCntByKey.get(key);
 
 								// retrieve relations
-								Map<String, Set<String>> relations = buildSenseRelations(senseRelationsByKey.get(k4));
+								var senseRelations2 = relationsByKey.get(key);
+								Map<String, Set<String>> relations = buildSenseRelations(senseRelations2);
 
 								// retrieve sensekey
-								String sensekey = sensekeyByKey.get(k3);
-								assert sensekey != null : "no sensekey for " + k3;
+								String sensekey = sensekeyByKey.get(key);
+								assert sensekey != null : "no sensekey for " + key;
 
 								// type
-								char type = pos != 'a' ? pos : (sensekey.split("%")[1].startsWith("5") ? 's' :'a');
+								char type = pos != 'a' ? pos : (sensekey.split("%")[1].startsWith("5") ? 's' : 'a');
 
 								// ver frames and adj positions
 								String[] verbFrames = pos != 'v' ? null : buildVerbFrames(synset, memberLemma);
 								String adjPosition = pos != 'a' ? null : (member.lemma instanceof AdjLemma ? ((AdjLemma) member.lemma).getPosition().getId() : null);
 
 								// collect lex
-								Key.W_P key = Key.W_P.from(memberLemma, type);
-								Lex lex = lexesByKey.computeIfAbsent(key, k -> new org.oewntk.model.Lex(memberLemma, Character.toString(type), null));
+								org.oewntk.model.Key.W_P wpKey = org.oewntk.model.Key.W_P.from(memberLemma, type);
+								Lex lex = lexesByKey.computeIfAbsent(wpKey, k -> new org.oewntk.model.Lex(memberLemma, Character.toString(type), null));
 
 								// lex senses
 								org.oewntk.model.Sense modelSense = new org.oewntk.model.Sense(sensekey, lex, pos, i[0], sense.synsetId.toString(), null, verbFrames, adjPosition, relations);
+								if (tagCnt.tagCount != 0)
+								{
+									modelSense.setTagCount(new org.oewntk.model.TagCount(tagCnt.senseNum, tagCnt.tagCount));
+								}
 								lex.addSense(modelSense);
 								senses.add(modelSense);
 							});
@@ -384,6 +277,12 @@ public class Parser
 				});
 	};
 
+	/**
+	 * Build sense relations
+	 *
+	 * @param relations relations including synset relations
+	 * @return sense relations
+	 */
 	private Map<String, Set<String>> buildSenseRelations(final Relation[] relations)
 	{
 		if (relations != null && relations.length > 0)
@@ -396,19 +295,31 @@ public class Parser
 		return null;
 	}
 
+	/**
+	 * Lex relation to target sensekey
+	 *
+	 * @param lr lexical relation
+	 * @return target sensekey
+	 */
 	private String toSensekey(final LexRelation lr)
 	{
 		SynsetId toSynsetId = lr.getToSynsetId();
 		Synset toSynset = pojoSynsetsById.get(toSynsetId);
 		LemmaRef toWordRef = lr.getToWord();
 		String toWord = toWordRef.resolve(toSynset).toString();
-		Key3 k3 = new Key3(toWord, toSynsetId.getPos().toChar(), toSynsetId.getOffset());
-		// Key4 k4 = new Key4(toWord, toSynsetId.getPos().toChar(), toSynsetId.getOffset(), toWordRef.getWordNum());
-		String sensekey = sensekeyByKey.get(k3);
-		assert sensekey != null : "no sensekey for " + k3;
+		Key key = new Key(toWord, toSynsetId.getPos().toChar(), toSynsetId.getOffset());
+		String sensekey = sensekeyByKey.get(key);
+		assert sensekey != null : "no sensekey for " + key;
 		return sensekey;
 	}
 
+	/**
+	 * Build verb frame ids
+	 *
+	 * @param synset synset
+	 * @param lemma  lemma
+	 * @return array of verb frame ids
+	 */
 	static String[] buildVerbFrames(Synset synset, String lemma)
 	{
 		var verbFrames = synset.getVerbFrames();
@@ -427,8 +338,14 @@ public class Parser
 	// 4 - C O N S U M E   M O R P H M A P P I N G   P O J O S
 	// from (noun|verb|adj|adv).exc
 
+	/**
+	 * Lemma to set of morphs
+	 */
 	private final Map<String, Map<Character, TreeSet<String>>> lemmaToMorphs = new HashMap<>();
 
+	/**
+	 * Morph consumer
+	 */
 	private final Consumer<MorphMapping> morphConsumer = mapping -> {
 
 		char pos = mapping.pos.toChar();
@@ -440,6 +357,29 @@ public class Parser
 						.computeIfAbsent(pos, p -> new TreeSet<>()) //
 						.add(morph));
 	};
+
+	/**
+	 * Inject morphs into model
+	 *
+	 * @param model         model
+	 * @param lemmaToMorphs lemma to morphs map
+	 */
+	private void setMorphs(final CoreModel model, final Map<String, Map<Character, TreeSet<String>>> lemmaToMorphs)
+	{
+		var lexByLemma = model.getLexesByLemma();
+		lemmaToMorphs.forEach((lemma, map2) -> //
+				map2.forEach((pos, morphs) -> {
+
+					var lexes = lexByLemma.get(lemma);
+					if (lexes != null)
+					{
+						lexes.stream().filter(lex -> lex.getPartOfSpeech() == pos).forEach(lex -> {
+							var morphs2 = morphs.toArray(String[]::new);
+							lex.setForms(morphs2);
+						});
+					}
+				}));
+	}
 
 	/**
 	 * WN dict directory
@@ -456,6 +396,17 @@ public class Parser
 		this.dir = dir;
 	}
 
+	/**
+	 * Parse core model
+	 *
+	 * @param synsetConsumer synset consumer
+	 * @param senseConsumer  sense consumer
+	 * @param indexConsumer  index consumer
+	 * @param morphConsumer  morph consumer
+	 * @return core model
+	 * @throws IOException        io exception
+	 * @throws ParsePojoException parse pojo exception
+	 */
 	public CoreModel parseCoreModel(final Consumer<Synset> synsetConsumer, final Consumer<Sense> senseConsumer, final Consumer<Index> indexConsumer, final Consumer<MorphMapping> morphConsumer) throws IOException, ParsePojoException
 	{
 		DataParser.parseAllSynsets(dir, synsetConsumer);
@@ -468,25 +419,15 @@ public class Parser
 		return model;
 	}
 
+	/**
+	 * Parse core model
+	 *
+	 * @return core model
+	 * @throws IOException        io exception
+	 * @throws ParsePojoException parse pojo exception
+	 */
 	public CoreModel parseCoreModel() throws IOException, ParsePojoException
 	{
 		return parseCoreModel(synsetConsumer, senseConsumer, indexConsumer, morphConsumer);
-	}
-
-	private void setMorphs(final CoreModel model, final Map<String, Map<Character, TreeSet<String>>> lemmaToMorphs)
-	{
-		var lexByLemma = model.getLexesByLemma();
-		lemmaToMorphs.forEach((lemma, map2) -> //
-				map2.forEach((pos, morphs) -> {
-
-					var lexes = lexByLemma.get(lemma);
-					if (lexes != null)
-					{
-						lexes.stream().filter(lex -> lex.getPartOfSpeech() == pos).forEach(lex -> {
-							var morphs2 = morphs.toArray(String[]::new);
-							lex.setForms(morphs2);
-						});
-					}
-				}));
 	}
 }
