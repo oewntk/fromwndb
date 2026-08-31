81795c2dda3c186c76cb0f9e37952447bbf57539
1313ou <1313ou@gmail.com>
2026-08-31 15:40:25 +0200

    remove LowerCasedLemma

--- src/test/java/org/oewntk/wndb/in/TestsWndbModelLexGroups.kt
+++ src/test/java/org/oewntk/wndb/in/TestsWndbModelLexGroups.kt
@@ -16,7 +16,6 @@ import org.oewntk.model.LibTestModelLexGroups.testCILemmas
 import org.oewntk.model.LibTestModelLexGroups.testCILexesFor
 import org.oewntk.model.LibTestModelLexGroups.testCILexesFor3
 import org.oewntk.model.LibTestModelLexGroups.testCIMultipleAll
-import org.oewntk.model.LowerCasedLemma
 import org.oewntk.wndb.`in`.LibTestsWndbCommon.model
 import org.oewntk.wndb.`in`.LibTestsWndbCommon.ps
 
